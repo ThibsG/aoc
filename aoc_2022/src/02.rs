8b9f86289462ac56f3a9a2675e49d5be7d4022d9
ThibsG <thibsg@pm.me>
2022-12-02 12:15:35 +0100

    02 - part 2

--- aoc_2022/src/02.rs
+++ aoc_2022/src/02.rs
@@ -1,5 +1,25 @@
 use std::str::FromStr;
 
+#[derive(Clone, Copy)]
+enum Ending {
+    Draw,
+    Lose,
+    Win,
+}
+
+impl FromStr for Ending {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(Self::Lose),
+            "Y" => Ok(Self::Draw),
+            "Z" => Ok(Self::Win),
+            _ => unimplemented!(),
+        }
+    }
+}
+
 /// Rock, Paper, Scissors
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd)]
 enum RPS {
@@ -24,6 +44,18 @@ impl RPS {
             | (Self::Rock, Self::Scissors)
             | (Self::Scissors, Self::Paper) => base_score + 6, // win
             _ => panic!("should be scored"),
+        }
+    }
+
+    fn score_with_ending(&self, ending: Ending) -> u8 {
+        match (ending, *self) {
+            (Ending::Draw, opponent) => self.score(opponent),
+            (Ending::Lose, Self::Rock) => Self::Scissors.score(Self::Rock),
+            (Ending::Lose, Self::Paper) => Self::Rock.score(Self::Paper),
+            (Ending::Lose, Self::Scissors) => Self::Paper.score(Self::Scissors),
+            (Ending::Win, Self::Scissors) => Self::Rock.score(Self::Scissors),
+            (Ending::Win, Self::Rock) => Self::Paper.score(Self::Rock),
+            (Ending::Win, Self::Paper) => Self::Scissors.score(Self::Paper),
         }
     }
 }
@@ -56,6 +88,20 @@ pub fn main() {
             me.score(opponent) as u32
         })
         .sum::<u32>();
-
     println!("Part 1 - Score: {score}");
+
+    // Second part
+    let score = input
+        .lines()
+        .map(|line| {
+            let line = line.split(' ').collect::<Vec<&str>>();
+            let (opponent, ending) = (
+                RPS::from_str(line[0]).unwrap(),
+                Ending::from_str(line[1]).unwrap(),
+            );
+            opponent.score_with_ending(ending) as u32
+        })
+        .sum::<u32>();
+
+    println!("Part 2 - Score: {score}");
 }
--- aoc_2022/src/main.rs
+++ aoc_2022/src/main.rs
@@ -1,6 +1,9 @@
 #[path = "01.rs"]
 mod _01;
+#[path = "02.rs"]
+mod _02;
 
 fn main() {
     _01::main();
+    _02::main();
 }
